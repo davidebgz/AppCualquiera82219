a4141daec8ab9ee6c0e1bf81da4c33da2c191708
david.gonzalez <david.gonzalez@CDS3UNIVO12>
2019-08-23 15:05:12 -0600

    a

---
+++ AppCualquiera82219/AppCualquiera82219/Model/CategoriaRepositorio.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AppCualquiera82219.Sevices;
+
+namespace AppCualquiera82219.Model
+{
+   public class CategoriaRepositorio
+    {
+
+    }
+}
--- AppCualquiera82219/AppCualquiera82219/Model/ProductoModel.cs
+++ AppCualquiera82219/AppCualquiera82219/Model/ProductoModel.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using SQLite;
 using SQLiteNetExtensions.Attributes;
-usingSQLiteNetExtensions.Extensions;
+using SQLiteNetExtensions.Extensions;
 
 namespace AppCualquiera82219.Model
 {
@@ -14,8 +14,12 @@ namespace AppCualquiera82219.Model
         public int Id{ get; set; }
         public string NameProduct{ get; set; }
         public decimal Price { get; set; }
-        [OneToMany(CascadeOperations =CascadeOperation.CascadeInsert)]
+       
         public int unit { get; set; }
-        
+        [ForeignKey(typeof(CategoriaModel))]
+        public int IdCategory { get; set; }
+        [ManyToOne]
+        public CategoriaModel Categoria { get; set; }
+
     }
 }
